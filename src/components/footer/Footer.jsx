1fc2bac400eb59a01a67ea15f59abeb7f11b37de
guzm4nCamila12 <mariaguramos12@gmail.com>
2024-06-26 10:46:43 -0500

    Terminando glossary

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -19,6 +19,30 @@ function Footer({ applyFilter = () => {} }) {
     );
 }
 
+{/*class Footer extends React.Component {
+    render() {
+      return (
+        <footer className="container-fluid bg-dark text-light py-3">
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <h1>Tecno Vocabulary</h1>
+            </div>
+            <div className="col-md-6">
+              <nav className="navbar navbar-expand-md navbar-dark justify-content-end nav">
+                <div className="navbar-nav">
+                  <a className="nav-link all" href="#" onClick={() => this.props.onFilterChange('ALL')}>ALL</a>
+                  {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter => (
+                    <a key={letter} className="nav-link letters" href="#" onClick={() => this.props.onFilterChange(letter)}>{letter}</a>
+                  ))}
+                </div>
+              </nav>
+            </div>
+          </div>
+        </footer>
+      );
+    }
+  }*/}
+
 export default Footer;
 
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import './Header.css';
+import Body from "../body/Body";
+import SettingsBackupRestoreIcon from '@mui/icons-material/SettingsBackupRestore';
 
 function Header() {
     return (
         <div className="header">
+            <button onClick={regresar} className="botonRegresar"> <SettingsBackupRestoreIcon/> </button>
             <h1>Glossary</h1>
         </div>
     )
+}
+
+function regresar(){
+    <p> <Body/> </p>
 }
 
 export default Header;
